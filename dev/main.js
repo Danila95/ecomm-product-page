5ee1aad89434f9c5d3db24aa9a9611ebb64fdc19
Danila <Danila00000@Gmail.com>
2021-02-21 12:23:06 +0700

    сверстал элементы &__buttons . Начал верстать элемент &__seze-selectors

--- dev/main.js
+++ dev/main.js
@@ -1,2 +1,11 @@
 import * as $ from 'jquery';
 import './style.scss';
+
+
+$('.buttons').on("change", function(e){
+    $('input[name^=field]').each(function(){
+        if(e.target != this){
+            this.checked = false;
+        }
+    });
+});
