11ba65678d8be76a5b0df2b8827a97d33d1a15f2
Danila <Danila00000@Gmail.com>
2021-02-22 13:44:00 +0700

    закончил верстать элементы &__seze-selectors &__additional-functions. Начал верстать элемент &__cart-buttons

--- dev/main.js
+++ dev/main.js
@@ -2,8 +2,16 @@ import * as $ from 'jquery';
 import './style.scss';
 
 
-$('.buttons').on("change", function(e){
+$('.buttons__checkbox').on("change", function(e){
     $('input[name^=field]').each(function(){
+        if(e.target != this){
+            this.checked = false;
+        }
+    });
+});
+
+$('.size-selectors__checkbox').on("change", function(e){
+    $('input[type=checkbox]').each(function(){
         if(e.target != this){
             this.checked = false;
         }
